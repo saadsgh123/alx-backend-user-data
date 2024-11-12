e8a8e34070b292c25a9cd6873cb5a188c31e7c85
saadsgh123 <sdsghouri@gmail.com>
2024-11-12 22:16:19 +0100

    task 3 try 2 🥶

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python3
-from typing import List
-
+"""
+Definition of class Auth
+"""
+from typing import (
+    List,
+    TypeVar
+)
 from flask import request
 
 
 class Auth:
-
+    """
+       Manages the API authentication
+    """
     @staticmethod
     def require_auth(path: str, excluded_paths: List[str]) -> bool:
+        """
+            Determines whether a given path requires authentication or not
+            Args:
+                - path(str): Url path to be checked
+                - excluded_paths(List of str): List of paths that do not require
+                    authentication
+            Return:
+                 True if path is not in excluded_paths, else False
+        """
         return False
 
     @staticmethod
     def authorization_header(request=None) -> str:
+        """
+            Returns the authorization header from a request object
+        """
         return None
 
     @staticmethod
     def current_user(request=None) -> TypeVar('User'):
+        """
+            Returns a User instance from information from a request object
+        """
         return None
--- 0x01-Basic_authentication/main.py
+++ 0x01-Basic_authentication/main.py
@@ -2,9 +2,9 @@
 """ Main 0
 """
 from api.v1.auth.auth import Auth
+if __name__ == '__main__':
+    a = Auth()
 
-a = Auth()
-
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.authorization_header())
-print(a.current_user())
+    print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+    print(a.authorization_header())
+    print(a.current_user())
