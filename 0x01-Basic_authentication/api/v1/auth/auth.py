1877030d87fa42df07a6ea78fd92c16a20773800
saadsgh123 <sdsghouri@gmail.com>
2024-11-12 22:55:40 +0100

    task 3 try 3 🥶 (pycodestyle)

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,35 +7,47 @@ from typing import (
     TypeVar
 )
 from flask import request
+from werkzeug.routing import NoMatch
 
 
 class Auth:
-    """
-       Manages the API authentication
-    """
-    @staticmethod
+
     def require_auth(path: str, excluded_paths: List[str]) -> bool:
         """
-            Determines whether a given path requires authentication or not
-            Args:
-            - path(str): Url path to be checked
-            - excluded_paths(List of str): List of paths that do not require
-                    authentication
-            Return:
-                 True if path is not in excluded_paths, else False
+        Returns the authorization header from a request object
+        :param path:
+        :param excluded_paths:
+        :return:
         """
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
 
     @staticmethod
-    def authorization_header(request=None) -> str:
+    def authorization_header() -> str:
         """
-            Returns the authorization header from a request object
+        Returns the authorization header from a request object
+        :return:
         """
         return None
 
-    @staticmethod
-    def current_user(request=None) -> TypeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User'):
         """
-            Returns a User instance from information from a request object
+        Returns a User instance from information from a request object
+        :param request:
+        :return:
         """
         return None
