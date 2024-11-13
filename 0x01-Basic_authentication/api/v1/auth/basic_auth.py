9111bdb33a141f452cc5757e8a78c723b4f001ac
saadsgh123 <sdsghouri@gmail.com>
2024-11-13 21:57:26 +0100

    task 8 try 1 🥶

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Definition of class Auth
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -23,3 +24,20 @@ class BasicAuth(Auth):
             return None
         token = authorization_header.split(" ")[-1]
         return token
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Decode a Base64-encoded string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64decode(decoded)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
