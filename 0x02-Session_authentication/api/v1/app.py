863cd09c9f019e40254132078a8f65b3b06fd3ad
saadsgh123 <sdsghouri@gmail.com>
2024-11-14 19:55:35 +0100

    task 0 try 1 😆

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,7 +39,7 @@ def bef_req():
             if auth.authorization_header(request) is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
-                abort(403, description="Forbidden")
+                request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
+from requests import request
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
@@ -25,12 +26,12 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id is None:
-        abort(404)
-    user = User.get(user_id)
-    if user is None:
-        abort(404)
-    return jsonify(user.to_json())
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        else:
+            user = User.get(user_id)
+            return jsonify(user)
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
