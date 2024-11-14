1c71e04b25d380aef95320d5df5df0681dcb9629
saadsgh123 <sdsghouri@gmail.com>
2024-11-14 21:13:46 +0100

    task 2 try 1 😆

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,8 @@
 """
 Definition of class Auth
 """
+import uuid
+
 from api.v1.auth.auth import Auth
 
 
@@ -9,4 +11,12 @@ class SessionAuth(Auth):
     """
     class definition
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None and isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
