778194f54c1d70b059c67ac8bbd1f3cf4c4db0e5
saadsgh123 <sdsghouri@gmail.com>
2024-11-14 20:21:59 +0100

    task 0 try 2 😆

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -4,7 +4,6 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
-from requests import request
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
