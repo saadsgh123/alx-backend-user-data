30fc2e3d90ec85f973ecfe3ac76a2c2d58491937
saadsgh123 <sdsghouri@gmail.com>
2024-11-19 20:04:01 +0100

    task 1 try 2 😆

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
         return self.__session
 
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Memoized session object
         """
         new_user = User(email=email, hashed_password=hashed_password)
