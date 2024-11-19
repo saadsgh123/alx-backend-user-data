43109ef92aca8766fd3813a2134aaead06e16562
saadsgh123 <sdsghouri@gmail.com>
2024-11-19 19:25:52 +0100

    task 0 try 2 😆

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    email = Column(String(250))
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250))
     session_id = Column(String(250))
     reset_token = Column(String(250))
