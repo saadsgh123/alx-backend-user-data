154b762972a95fc0a8254cd2c2e61d3709261449
saadsgh123 <sdsghouri@gmail.com>
2024-11-21 23:03:08 +0100

    task 12 try 2 😆

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,7 +48,8 @@ def login():
     is_valid = AUTH.valid_login(email, password)
     if is_valid:
         session_id = AUTH.create_session(email)
-        response = make_response(jsonify({"email": email, "message": "logged in"}))
+        response = make_response(
+            jsonify({"email": email, "message": "logged in"}))
         response.set_cookie("session_id", session_id)
         return response
     return abort(401)
