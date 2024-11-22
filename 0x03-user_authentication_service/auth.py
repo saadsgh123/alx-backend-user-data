d746b0ccdc0e43b6571a7f9858da898a5c1abfdb
saadsgh123 <sdsghouri@gmail.com>
2024-11-23 00:36:17 +0100

    task 19 try 2 😆

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -120,4 +120,7 @@ class Auth:
             raise ValueError()
 
         hashed_password = _hash_password(password)
-        self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
+        return None
