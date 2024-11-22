74cacd722f343342839e86a266232f6dcd54eb02
saadsgh123 <sdsghouri@gmail.com>
2024-11-22 22:38:48 +0100

    task 14 try 1 😆

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,13 @@ class Auth:
             except NoResultFound:
                 return None
             return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Function that removes session_id from the database
+        :param user_id:
+        :return:
+        """
+        user = self._db.find_user_by(id=user_id)
+        self._db.update_user(user.id, session_id=None)
+        return None
